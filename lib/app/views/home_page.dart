18300f1845a63a5e6a695ce958e48544756a535f
Caio-bergbjj <caioberg09@gmail.com>
2022-08-07 19:56:46 -0300

    Update home_page.dart
    Botton navigations

--- lib/app/views/home_page.dart
+++ lib/app/views/home_page.dart
@@ -23,33 +23,40 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(backgroundColor: Colors.white70,
-          title: OutlinedButton(onPressed: () {  },
-          child: const Icon(Icons.logout),),
-          actions: [
-        Container(
-          margin: const EdgeInsets.only(right: 20),
-          child: SizedBox(
-            width: 120,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text("57% Completo",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold)),
-                LinearProgressIndicator(
-                  value: 0.57,
-                  backgroundColor: Colors.red,
-                  color: Color.fromARGB(255, 89, 233, 95),
-                  semanticsLabel: 'Linear progress indicator',
-                ),
-              ],
+      appBar: AppBar(
+          backgroundColor: Colors.white70,
+          elevation: 0,
+          title: OutlinedButton(
+            onPressed: () {},
+            child: const Icon(
+              Icons.logout,
+              color: Colors.grey,
             ),
           ),
-        ),
-      ]),
+          actions: [
+            Container(
+              margin: const EdgeInsets.only(right: 20),
+              child: SizedBox(
+                width: 120,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text("57% Completo",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold)),
+                    LinearProgressIndicator(
+                      value: 0.57,
+                      backgroundColor: Colors.grey,
+                      color: Color.fromARGB(255, 89, 233, 95),
+                      semanticsLabel: 'Linear progress indicator',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ]),
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.fromLTRB(50, 50, 50, 50),
@@ -63,13 +70,15 @@ class _HomePageState extends State<HomePage> {
           ]),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home),
-        label: "teste1"),
-        BottomNavigationBarItem(icon: Icon(Icons.person),
-        label: "teste"),
-
-      ]),
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          elevation: 100,
+          selectedItemColor: Colors.blueAccent,
+          unselectedItemColor: Colors.black,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: ""),
+          ]),
     );
   }
 }
