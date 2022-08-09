0b92a72fee511a0d4a924eeae3f36ead96f82f0d
Mateuszinnn <mateusdealmeidadias@gmail.com>
2022-08-09 17:31:32 -0300

    Desenvolvimento da Tela singup_page_part1, versão 3.

--- lib/app/views/singup_page_part1.dart
+++ lib/app/views/singup_page_part1.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'components/paulaTitle.dart';
+import 'package:gender_picker/source/enums.dart';
+import 'package:gender_picker/source/gender_picker.dart';
 
 class SingupPage extends StatefulWidget {
   const SingupPage({Key? key}) : super(key: key);
@@ -15,9 +17,9 @@ class _SingupPageState extends State<SingupPage> {
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
           colors: [
-        Color.fromARGB(255, 100, 171, 226),
-        Color.fromARGB(255, 41, 171, 226)
-      ]));
+            Color.fromARGB(255, 100, 171, 226),
+            Color.fromARGB(255, 41, 171, 226)
+          ]));
 
   String nickname = '';
   String password = '';
@@ -31,8 +33,14 @@ class _SingupPageState extends State<SingupPage> {
     return Material(
       child: Container(
         decoration: BackgroundBlueGradiend,
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
+        width: MediaQuery
+            .of(context)
+            .size
+            .width,
+        height: MediaQuery
+            .of(context)
+            .size
+            .height,
         child: Column(
           children: [
             const Expanded(
@@ -70,6 +78,7 @@ class _SingupPageState extends State<SingupPage> {
                       height: 20,
                     ),
                     Expanded(
+
                       child: Container(
                         width: double.infinity,
                         color: Colors.transparent,
@@ -80,137 +89,178 @@ class _SingupPageState extends State<SingupPage> {
                                   topLeft: Radius.circular(40.0),
                                   topRight: Radius.circular(40.0),
                                 )),
-                            child: Column(
-                              children: [
-                                Center(
-                                  child: Column(
-                                    children: [
-                                      Container(
-                                        height: 20,
-                                      ),
-                                      const Text("Seu Nome",
-                                          style: TextStyle(
-                                              color: Colors.blueAccent,
-                                              fontSize: 20,
-                                              fontFamily: "Nunito",
-                                              fontWeight: FontWeight.w500)),
-                                      Container(
-                                        decoration:
-                                            const BoxDecoration(boxShadow: [
-                                          BoxShadow(
-                                            color: Color.fromARGB(50, 0, 0, 0),
-                                            blurRadius: 15,
-                                            offset: Offset(0, 5),
-                                          ),
-                                        ]),
-                                        child: TextField(
-                                          decoration: InputDecoration(
-                                            contentPadding:
-                                                const EdgeInsets.symmetric(
-                                                    vertical: 10,
-                                                    horizontal: 4),
-                                            isDense: true,
-                                            hintText: '****',
-                                            fillColor: Colors.white,
-                                            filled: true,
-                                            border: OutlineInputBorder(
-                                              borderSide: BorderSide.none,
-                                              borderRadius:
-                                                  BorderRadius.circular(8),
-                                            ),
-                                          ),
-                                          obscureText: true,
-                                          style: const TextStyle(
-                                              color: Colors.black),
-                                          onChanged: (value) {
-                                            password = value;
-                                          },
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+                              child: Column(
+                                children: [
+                                  Center(
+                                    child: Column(
+                                      children: [
+                                        Container(
+                                          height: 20,
                                         ),
-                                      ),
-                                      Container(
-                                        height: 20,
-                                      ),
-                                      const Text("Profissão",
-                                          style: TextStyle(
-                                              color: Colors.blueAccent,
-                                              fontSize: 20,
-                                              fontFamily: "Nunito",
-                                              fontWeight: FontWeight.w500)),
-                                      Container(
-                                        decoration:
-                                            const BoxDecoration(boxShadow: [
-                                          BoxShadow(
-                                            color: Color.fromARGB(50, 0, 0, 0),
-                                            blurRadius: 15,
-                                            offset: Offset(0, 5),
+                                        Padding(
+                                          padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                                          child: Align(
+                                            alignment: Alignment.centerLeft,
+                                            child: Text("Seu Nome",
+                                                style: TextStyle(
+                                                    color: Colors.blueAccent,
+                                                    fontSize: 20,
+                                                    fontFamily: "Nunito",
+                                                    fontWeight: FontWeight.w500)),
                                           ),
-                                        ]),
-                                        child: TextField(
-                                          decoration: InputDecoration(
-                                            contentPadding:
-                                                const EdgeInsets.symmetric(
-                                                    vertical: 10,
-                                                    horizontal: 6),
-                                            isDense: true,
-                                            hintText: '****',
-                                            fillColor: Colors.white,
-                                            filled: true,
-                                            border: OutlineInputBorder(
-                                              borderSide: BorderSide.none,
-                                              borderRadius:
-                                                  BorderRadius.circular(8),
-                                            ),
-                                          ),
-                                          obscureText: true,
-                                          style: const TextStyle(
-                                              color: Colors.black),
-                                          onChanged: (value) {
-                                            password = value;
-                                          },
                                         ),
-                                      ),
-                                      Container(
-                                        height: 20,
-                                      ),
-                                      FloatingActionButton.extended(
-                                        onPressed: () {
-                                          DatePicker.showDatePicker(context,
-                                              showTitleActions: true,
-                                              minTime: DateTime(1910, 1, 1),
-                                              maxTime: DateTime(2022, 12, 31), onChanged: (date) {
-                                                print('change $date');
-                                                setState(() {
-                                                  pickeddate = "${date.day}";
-                                                });
-                                              }, onConfirm: (date) {
-                                                print('confirm $date');
-                                                setState(() {
-                                                  pickeddate =
-                                                  "Picked Date is : ${date.day}/${date.month}/${date.year}";
-                                                });
-                                              }, currentTime: DateTime.now());
+                                        Container(
+                                          decoration:
+                                          const BoxDecoration(boxShadow: [
+                                            BoxShadow(
+                                              color: Color.fromARGB(50, 0, 0, 0),
+                                              blurRadius: 15,
+                                              offset: Offset(0, 5),
+                                            ),
+                                          ]),
+                                          child: TextField(
+                                            decoration: InputDecoration(
+                                              contentPadding:
+                                              const EdgeInsets.symmetric(
+                                                  vertical: 10,
+                                                  horizontal: 4),
+                                              isDense: true,
+                                              hintText: '****',
+                                              fillColor: Colors.white,
+                                              filled: true,
+                                              border: OutlineInputBorder(
+                                                borderSide: BorderSide.none,
+                                                borderRadius:
+                                                BorderRadius.circular(8),
+                                              ),
+                                            ),
+                                            obscureText: true,
+                                            style: const TextStyle(
+                                                color: Colors.black),
+                                            onChanged: (value) {
+                                              password = value;
+                                            },
+                                          ),
+                                        ),
+                                        Container(
+                                          height: 20,
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                                          child: Align(
+                                            alignment: Alignment.centerLeft,
+                                            child: Text("Profissão",
+                                                style: TextStyle(
+                                                    color: Colors.blueAccent,
+                                                    fontSize: 20,
+                                                    fontFamily: "Nunito",
+                                                    fontWeight: FontWeight.w500)),
+                                          ),
+                                        ),
+                                        Container(
+                                          decoration:
+                                            BoxDecoration(boxShadow: [
+                                            BoxShadow(
+                                              color: Color.fromARGB(50, 0, 0, 0),
+                                              blurRadius: 15,
+                                              offset: Offset(0, 5),
+                                            ),
+                                          ]),
+                                          child: TextField(
+                                            decoration: InputDecoration(
+                                              contentPadding:
+                                              const EdgeInsets.symmetric(
+                                                  vertical: 10,
+                                                  horizontal: 6),
+                                              isDense: true,
+                                              fillColor: Colors.white,
+                                              filled: true,
 
-                                        },
-                                        label: Text("Data de Nascismento,",
+                                              border: OutlineInputBorder(
+                                                borderSide: BorderSide.none,
+                                                borderRadius:
+                                                BorderRadius.circular(8),
+                                              ),
+                                            ),
+                                            obscureText: true,
+                                            style: const TextStyle(
+                                                color: Colors.black),
+                                            onChanged: (value) {
+                                              password = value;
+                                            },
+                                          ),
+                                        ),
+                                        Container(
+                                          height: 20,
+                                        ),
+
+                                        FloatingActionButton.extended(
+                                          onPressed: () {
+                                            DatePicker.showDatePicker(context,
+                                                showTitleActions: true,
+                                                minTime: DateTime(1910, 1, 1),
+                                                maxTime: DateTime(2022, 12, 31),
+                                                onChanged: (date) {
+                                                  print('change $date');
+                                                  setState(() {
+                                                    pickeddate = "${date.day}";
+                                                  });
+                                                },
+                                                onConfirm: (date) {
+                                                  print('confirm $date');
+                                                  setState(() {
+                                                    pickeddate =
+                                                    "Picked Date is : ${date
+                                                        .day}/${date.month}/${date
+                                                        .year}";
+                                                  });
+                                                },
+                                                currentTime: DateTime.now());
+                                          },
+                                          label: Text(
+                                            "Data de Nascimento",
                                             style: TextStyle(
                                                 color: Colors.blueAccent,
                                                 fontSize: 20,
                                                 fontFamily: "Nunito",
                                                 fontWeight: FontWeight.w500),
+                                          ),
+                                          icon: Icon(Icons.date_range),
+                                          backgroundColor: Colors.white,
                                         ),
-                                        icon: Icon(Icons.date_range),
-                                        backgroundColor: Color.fromARGB(255, 100, 171, 226),
+                                        Container(
+                                          height: 20,
+                                        ),
+                                        SizedBox(
+                                          width: 140,
+                                          height: 40,
+                                          child: ElevatedButton(
+                                            onPressed: () {
 
-
-                                      ),
-
-                                      Container(
-                                        height: 20,
-                                      ),
-                                    ],
-                                  ),
-                                )
-                              ],
+                                            },
+                                            onHover: (hover) {},
+                                            style: ButtonStyle(
+                                              foregroundColor:
+                                              MaterialStateProperty.all<Color>(Colors.white),
+                                              backgroundColor:
+                                              MaterialStateProperty.all<Color>(Colors.blue),
+                                              shape:
+                                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                                  RoundedRectangleBorder(
+                                                      borderRadius: BorderRadius.circular(10),
+                                                      side: BorderSide.none)),
+                                            ),
+                                            child: const Text("Próximo",
+                                                style: TextStyle(fontSize: 20)),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
                             )),
                       ),
                     )
