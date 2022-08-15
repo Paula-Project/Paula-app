9e4d2e043622e39b049bc34e9a71bb1c6d57a616
emersonteles <emersonteles21@gmail.com>
2022-08-15 00:20:44 -0300

    refactor: componentizing dialog text box

--- lib/app/views/welcome_page_part2.dart
+++ lib/app/views/welcome_page_part2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'components/DIalogTextBox.dart';
 
 class WelcomePage_part2 extends StatelessWidget {
   const WelcomePage_part2({Key? key}) : super(key: key);
@@ -42,34 +43,9 @@ class WelcomePage_part2 extends StatelessWidget {
                       'assets/images/Avatar-Maker(2).png',
                       width: MediaQuery.of(context).size.width - 150,
                     ),
-                    Row(
-                      children: [
-                        ClipPath(
-                          clipper: CustomTriangleClipper(),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 15),
-                            child: Container(
-                              width: 25,
-                              height: 25,
-                              decoration: const BoxDecoration(
-                                  color: Color.fromARGB(199, 37, 85, 124)),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          color: const Color.fromARGB(199, 37, 85, 124),
-                          borderRadius: BorderRadius.circular(15)),
-                      child: const Padding(
-                        padding: EdgeInsets.all(10.0),
-                        child: Text(
+                    const DialogTextBox(
+                      TextContent:
                           "Oi, eu me chamo Paula, e eu vou acompanhar o seu aprendizado de perto!",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w300),
-                        ),
-                      ),
                     ),
                     const SizedBox(height: 30),
                     SizedBox(
@@ -101,21 +77,5 @@ class WelcomePage_part2 extends StatelessWidget {
         ]),
       ),
     );
-  }
-}
-
-class CustomTriangleClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    final path = Path();
-    path.lineTo(0, size.height);
-    path.lineTo(size.width, size.height);
-    path.lineTo(0, 0);
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return false;
   }
 }
