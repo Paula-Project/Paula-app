09c561fb04fe1f4a9dc9d79f21c45713d11cc86a
Emerson Teles <54421630+EmersonTeles@users.noreply.github.com>
2022-08-16 00:44:19 -0300

    Feature/lessons (#12)
    * adding new images
    
    * feat: card selection
    
    * refactor: turning card image into a component
    
    * removing profission

--- lib/app/views/lessons/task_page.dart
+++ lib/app/views/lessons/task_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
-import 'package:page_transition/page_transition.dart';
 import 'package:paula/app/views/components/ButtonNext.dart';
+import 'package:paula/app/views/components/CardImage.dart';
 import '../home_page.dart';
 
 class TaskPage extends StatefulWidget {
@@ -12,6 +11,8 @@ class TaskPage extends StatefulWidget {
 }
 
 class _TaskPageState extends State<TaskPage> {
+  var cardSelected = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +23,7 @@ class _TaskPageState extends State<TaskPage> {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 40.0, horizontal: 25.0),
+          padding: const EdgeInsets.symmetric(vertical: 40.0, horizontal: 15.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -49,19 +50,33 @@ class _TaskPageState extends State<TaskPage> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Column(
-                    children: const <Widget>[
-                      AudioButton(
+                    children: <Widget>[
+                      CardImage(
+                        imageUrl: 'assets/images/oculos.png',
+                        scale: 5.0,
                         audioUrl: 'oculos.mp3',
+                        isSelected: cardSelected == 1 ? true : false,
+                        onPress: () {
+                          setState(() {
+                            cardSelected = 1;
+                          });
+                        },
                       ),
-                      CardImage('assets/images/oculos.png', 5.0),
                     ],
                   ),
                   Column(
-                    children: const <Widget>[
-                      AudioButton(
+                    children: <Widget>[
+                      CardImage(
+                        imageUrl: 'assets/images/uva.png',
                         audioUrl: 'uva.mp3',
+                        scale: 8.0,
+                        isSelected: cardSelected == 2 ? true : false,
+                        onPress: () {
+                          setState(() {
+                            cardSelected = 2;
+                          });
+                        },
                       ),
-                      CardImage('assets/images/uva.png', 6.0),
                     ],
                   ),
                 ],
@@ -71,34 +86,39 @@ class _TaskPageState extends State<TaskPage> {
                 children: [
                   Column(
                     children: [
-                      const AudioButton(
+                      CardImage(
+                        imageUrl: 'assets/images/arvore.png',
                         audioUrl: 'arvore.mp3',
-                      ),
-                      MaterialButton(
-                        onPressed: () {
-                          Navigator.of(context).pushAndRemoveUntil(
-                              PageTransition(
-                                  type: PageTransitionType.leftToRight,
-                                  child: const HomePage()),
-                              (route) => false);
+                        scale: 5.0,
+                        isSelected: cardSelected == 3 ? true : false,
+                        onPress: () {
+                          setState(() {
+                            cardSelected = 3;
+                          });
                         },
-                        child: const CardImage('assets/images/arvore.png', 5.0),
                       ),
                     ],
                   ),
                   Column(
-                    children: const <Widget>[
-                      AudioButton(
+                    children: <Widget>[
+                      CardImage(
+                        imageUrl: 'assets/images/escada.png',
+                        scale: 8.0,
                         audioUrl: 'escada.mp3',
+                        isSelected: cardSelected == 4 ? true : false,
+                        onPress: () {
+                          setState(() {
+                            cardSelected = 4;
+                          });
+                        },
                       ),
-                      CardImage('assets/images/escada.png', 6.0),
                     ],
                   ),
                 ],
               ),
               Container(
                 margin: const EdgeInsets.only(top: 30.0),
-                child: SizedBox(
+                child: const SizedBox(
                     width: 200,
                     height: 40,
                     child: ButtonNext(
@@ -110,76 +130,5 @@ class _TaskPageState extends State<TaskPage> {
         ),
       ),
     );
-  }
-}
-
-class AudioButton extends StatefulWidget {
-  final String audioUrl;
-  const AudioButton({
-    Key? key,
-    required this.audioUrl,
-  }) : super(key: key);
-
-  @override
-  State<AudioButton> createState() => _AudioButtonState();
-}
-
-class _AudioButtonState extends State<AudioButton> {
-  AudioPlayer? audioPlayer;
-
-  _runAudio(String path) async {
-    try {
-      await audioPlayer?.play(AssetSource(path));
-    } catch (error) {
-      print(error.toString());
-    }
-  }
-
-  @override
-  void initState() {
-    audioPlayer = AudioPlayer();
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialButton(
-      onPressed: () async {
-        _runAudio("audios/${widget.audioUrl}");
-      },
-      child: Container(
-        margin: const EdgeInsets.fromLTRB(0, 20, 0, 10),
-        height: 30.00,
-        width: 30.00,
-        decoration: const BoxDecoration(
-            color: Color.fromRGBO(37, 85, 124, 1),
-            borderRadius: BorderRadius.all(Radius.circular(5.0))),
-        child: const Icon(
-          Icons.volume_up,
-        ),
-      ),
-    );
-  }
-}
-
-class CardImage extends StatelessWidget {
-  const CardImage(this.imageUrl, this.scale, {Key? key}) : super(key: key);
-  final String imageUrl;
-  final double scale;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        height: (MediaQuery.of(context).size.height) * 0.17,
-        width: (MediaQuery.of(context).size.width) * 0.38,
-        decoration: const BoxDecoration(
-            color: Color.fromRGBO(209, 220, 221, 1),
-            borderRadius: BorderRadius.all(
-              Radius.circular(15.0),
-            )),
-        child: Image.asset(
-          imageUrl,
-          scale: scale,
-        ));
   }
 }
