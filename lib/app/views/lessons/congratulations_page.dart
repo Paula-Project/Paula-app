4a43f597383e88779402aac524491ca4a1df7082
emersonteles <emersonteles21@gmail.com>
2022-08-16 01:13:15 -0300

    feat: linking lessons each other

--- lib/app/views/lessons/congratulations_page.dart
+++ lib/app/views/lessons/congratulations_page.dart
@@ -20,83 +20,71 @@ class _CongratulationsPageState extends State<CongratulationsPage> {
           color: Colors.white,
         ),
         height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width ,
+        width: MediaQuery.of(context).size.width,
         child: Column(
-
-
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(30,100,30,10),
-            child: Container(
-              height: (MediaQuery.of(context).size.height) * 0.6,
-              width: (MediaQuery.of(context).size.width) * 0.8,
-
-              decoration: const BoxDecoration(
-                color: Color.fromRGBO(37, 85, 124, 1),
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30, 100, 30, 10),
+              child: Container(
+                height: (MediaQuery.of(context).size.height) * 0.6,
+                width: (MediaQuery.of(context).size.width) * 0.8,
+                decoration: const BoxDecoration(
+                  color: Color.fromRGBO(37, 85, 124, 1),
                   borderRadius: BorderRadius.all(Radius.circular(15)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 10.0,
-                    spreadRadius: 2.0,
-
-                  )
-
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 10.0,
+                      spreadRadius: 2.0,
+                    )
                   ],
-
-              ),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(3.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Image.asset('images/fireworks.png', scale: 6.0),
-                        Image.asset('images/fireworks.png', scale: 6.0),
-                      ],
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(3.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Image.asset('images/fireworks.png', scale: 6.0),
+                          Image.asset('images/fireworks.png', scale: 6.0),
+                        ],
+                      ),
                     ),
-                  ),
-              const Padding(
-                padding: EdgeInsets.fromLTRB(8,40,8,0),
-                child: Text(
-                  "VOCÊ ESTÁ \n INDO MUITO \n BEM!",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: 'Poppins',
-
-                    color: Colors.white,
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-
-                  ),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(8, 40, 8, 0),
+                      child: Text(
+                        "VOCÊ ESTÁ \n INDO MUITO \n BEM!",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30),
+                      child: Image.asset('images/fireworks.png', scale: 6.0),
+                    ),
+                  ],
                 ),
               ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30),
-                    child: Image.asset('images/fireworks.png', scale: 6.0),
-                  ),
-
-                ],
-              ),
-
-
             ),
-          ),
-
-          Container(
-            margin: const EdgeInsets.only(top: 70.0),
-            child: const SizedBox(
-                width: 350,
-                height: 50,
-                child: ButtonNext(
-                  pageWidget: HomePage(),
-                )),
-          ),
-        ],
+            Container(
+              margin: const EdgeInsets.only(top: 70.0),
+              child: const SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: ButtonNext(
+                    pageWidget: HomePage(),
+                    allowedReturn: false,
+                  )),
+            ),
+          ],
+        ),
       ),
-      ),
-
     );
   }
 }
