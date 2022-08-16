7630ccf95536ad693805ae4efc59327f3d7ed9f9
emersonteles <emersonteles21@gmail.com>
2022-08-16 00:26:28 -0300

    refactor: turning card image into a component

--- lib/app/views/lessons/task_page.dart
+++ lib/app/views/lessons/task_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
-import 'package:page_transition/page_transition.dart';
 import 'package:paula/app/views/components/ButtonNext.dart';
+import 'package:paula/app/views/components/CardImage.dart';
 import '../home_page.dart';
 
 class TaskPage extends StatefulWidget {
@@ -119,7 +118,7 @@ class _TaskPageState extends State<TaskPage> {
               ),
               Container(
                 margin: const EdgeInsets.only(top: 30.0),
-                child: SizedBox(
+                child: const SizedBox(
                     width: 200,
                     height: 40,
                     child: ButtonNext(
@@ -130,66 +129,6 @@ class _TaskPageState extends State<TaskPage> {
           ),
         ),
       ),
-    );
-  }
-}
-
-class CardImage extends StatefulWidget {
-  const CardImage({
-    Key? key,
-    required this.imageUrl,
-    required this.scale,
-    required this.isSelected,
-    required this.audioUrl,
-    required this.onPress,
-  }) : super(key: key);
-  final String audioUrl;
-  final String imageUrl;
-  final double scale;
-  final bool isSelected;
-  final VoidCallback onPress;
-  @override
-  State<CardImage> createState() => _CardImageState();
-}
-
-class _CardImageState extends State<CardImage> {
-  AudioPlayer? audioPlayer;
-
-  _runAudio(String path) async {
-    try {
-      await audioPlayer?.play(AssetSource(path));
-    } catch (error) {
-      print(error.toString());
-    }
-  }
-
-  @override
-  void initState() {
-    audioPlayer = AudioPlayer();
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialButton(
-      onPressed: (() {
-        _runAudio("audios/${widget.audioUrl}");
-        widget.onPress();
-      }),
-      child: Container(
-          height: (MediaQuery.of(context).size.height) * 0.17,
-          width: (MediaQuery.of(context).size.width) * 0.38,
-          decoration: BoxDecoration(
-              color: widget.isSelected
-                  ? Colors.blue
-                  : const Color.fromRGBO(209, 220, 221, 1),
-              borderRadius: const BorderRadius.all(
-                Radius.circular(15.0),
-              )),
-          child: Image.asset(
-            widget.imageUrl,
-            scale: widget.scale,
-          )),
     );
   }
 }
