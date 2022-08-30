6945b45142c462f8c072e50f8669631151ee468a
emersonteles <emersonteles21@gmail.com>
2022-08-30 01:01:41 -0300

    task dialog improviments:

--- lib/app/views/components/BoxDialog.dart
+++ lib/app/views/components/BoxDialog.dart
@@ -1,12 +1,41 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:paula/app/views/lessons/congratulations_page.dart';
 
-class BoxDialog extends StatelessWidget {
-  const BoxDialog({Key? key, required this.feedback, required this.resposta})
+class BoxDialog extends StatefulWidget {
+  BoxDialog({Key? key, required this.feedback, required this.resposta})
       : super(key: key);
 
   final String resposta;
   final bool feedback;
+
+  @override
+  State<BoxDialog> createState() => _BoxDialogState();
+}
+
+class _BoxDialogState extends State<BoxDialog> {
+  final AudioPlayer? audioPlayer = AudioPlayer();
+
+  _runAudio(String path) async {
+    try {
+      await audioPlayer?.play(AssetSource(path));
+    } catch (error) {
+      print(error.toString());
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (widget.feedback) {
+        _runAudio("audios/sucess_sound.mp3");
+      } else {
+        _runAudio("audios/failed_sound.mp3");
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,17 +43,25 @@ class BoxDialog extends StatelessWidget {
     String messageButton = 'CONTINUAR';
     Color color = Colors.green;
     double padding = 15.0;
-    if (!feedback) {
-      message = 'Incorreto \n Resposta: $resposta';
+
+    if (!widget.feedback) {
+      message = 'Incorreto \n Resposta: ${widget.resposta}';
       messageButton = 'TENTAR DE NOVO';
       color = Colors.red;
       padding = 0;
     }
 
     return AlertDialog(
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(40), topRight: Radius.circular(40))),
       insetPadding: EdgeInsets.zero,
+      buttonPadding: const EdgeInsets.all(25.0),
+      contentPadding: const EdgeInsets.only(top: 25.0),
       backgroundColor: Colors.white,
       alignment: Alignment.bottomCenter,
+      actionsAlignment: MainAxisAlignment.center,
+      semanticLabel: "dialogg",
       content: Builder(
         builder: (context) {
           // Get available height and width of the build area of this widget. Make a choice depending on the size.
@@ -33,7 +70,6 @@ class BoxDialog extends StatelessWidget {
 
           return Container(
             padding: EdgeInsets.only(top: padding),
-            height: height * 0.08,
             width: width,
             child: Text(
               message,
@@ -44,7 +80,6 @@ class BoxDialog extends StatelessWidget {
           );
         },
       ),
-      actionsAlignment: MainAxisAlignment.center,
       actions: <Widget>[
         ElevatedButton(
           onPressed: () {
