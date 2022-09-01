a5d6345b723ab8bb848e49005dfa910cc0f18add
Mateuszinnn <mateusdealmeidadias@gmail.com>
2022-09-01 20:45:09 -0300

    Mudança de página com o delizar do dedo.

--- lib/app/views/welcome_page_part2.dart
+++ lib/app/views/welcome_page_part2.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:paula/app/views/welcome_page_part1.dart';
 import 'components/DIalogTextBox.dart';
+import 'welcome_page_part3.dart';
+import 'welcome_page_part1.dart';
+
 
 import 'components/paulaTitle.dart';
 
@@ -9,66 +14,82 @@ class WelcomePage_part2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color.fromARGB(200, 100, 171, 226),
-              Color.fromARGB(255, 41, 171, 226),
-            ],
+      child: GestureDetector(
+        onPanUpdate: (details){
+          if(details.delta.dx < 0)
+            Navigator.push(
+                context,
+                PageTransition(
+                    type: PageTransitionType.rightToLeft,
+                    child: const WelcomePage_part3()));
+          if(details.delta.dx >0)
+            Navigator.push(
+                context,
+                PageTransition(
+                    type: PageTransitionType.leftToRight,
+                    child: const WelcomePage_part1()));
+        },
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color.fromARGB(200, 100, 171, 226),
+                Color.fromARGB(255, 41, 171, 226),
+              ],
+            ),
           ),
-        ),
-        child: Column(children: [
-          const Expanded(
-            flex: 3,
-            child: PaulaTitleComponent(),
-          ),
-          Expanded(
-              flex: 8,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Image.asset(
-                      'assets/images/Avatar-Maker(2).png',
-                      width: MediaQuery.of(context).size.width - 150,
-                    ),
-                    const DialogTextBox(
-                      TextContent:
-                          "Oi, eu me chamo Paula, e eu vou acompanhar o seu aprendizado de perto!",
-                      audioUrl: "paula01.mp3",
-                    ),
-                    const SizedBox(height: 30),
-                    SizedBox(
-                      width: 140,
-                      height: 40,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, "/welcome_part3");
-                        },
-                        onHover: (hover) {},
-                        style: ButtonStyle(
-                          foregroundColor:
-                              MaterialStateProperty.all<Color>(Colors.blue),
-                          backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.white),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                      side: BorderSide.none)),
-                        ),
-                        child: const Text("Próximo",
-                            style: TextStyle(fontSize: 20)),
+          child: Column(children: [
+            const Expanded(
+              flex: 3,
+              child: PaulaTitleComponent(),
+            ),
+            Expanded(
+                flex: 8,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                        'assets/images/Avatar-Maker(2).png',
+                        width: MediaQuery.of(context).size.width - 150,
                       ),
-                    ),
-                  ],
-                ),
-              ))
-        ]),
+                      const DialogTextBox(
+                        TextContent:
+                            "Oi, eu me chamo Paula, e eu vou acompanhar o seu aprendizado de perto!",
+                        audioUrl: "paula01.mp3",
+                      ),
+                      const SizedBox(height: 30),
+                      SizedBox(
+                        width: 140,
+                        height: 40,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, "/welcome_part3");
+                          },
+                          onHover: (hover) {},
+                          style: ButtonStyle(
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.blue),
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            shape:
+                                MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                        side: BorderSide.none)),
+                          ),
+                          child: const Text("Próximo",
+                              style: TextStyle(fontSize: 20)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ))
+          ]),
+        ),
       ),
     );
   }
--- lib/app/views/welcome_page_part3.dart
+++ lib/app/views/welcome_page_part3.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:paula/app/views/components/DIalogTextBox.dart';
 import 'package:paula/app/views/singup_page_part1.dart';
+import 'welcome_page_part2.dart';
 
 import 'components/paulaTitle.dart';
 
@@ -9,72 +11,89 @@ class WelcomePage_part3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color.fromARGB(200, 100, 171, 226),
-              Color.fromARGB(255, 41, 171, 226),
-            ],
+    return GestureDetector(
+      onPanUpdate: (details){
+        if(details.delta.dx < 0)
+          Navigator.push(
+              context,
+              PageTransition(
+                  type: PageTransitionType.rightToLeft,
+                  child: const SingupPage()));
+        if(details.delta.dx > 0)
+          Navigator.push(
+              context,
+              PageTransition(
+                  type: PageTransitionType.leftToRight,
+                  child: const WelcomePage_part2()));
+      },
+      child: Material(
+
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color.fromARGB(200, 100, 171, 226),
+                Color.fromARGB(255, 41, 171, 226),
+              ],
+            ),
           ),
-        ),
-        child: Column(children: [
-          const Expanded(
-            flex: 3,
-            child: PaulaTitleComponent(),
-          ),
-          Expanded(
-              flex: 8,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Image.asset(
-                      'assets/images/Avatar-Maker(2).png',
-                      width: MediaQuery.of(context).size.width - 150,
-                    ),
-                    const DialogTextBox(
-                      TextContent:
-                          "Para começar vamos precisar de algumas informações suas, \ntudo bem?",
-                      audioUrl: 'paula02.ogg',
-                    ),
-                    const SizedBox(height: 30),
-                    SizedBox(
-                      width: 140,
-                      height: 40,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (BuildContext context) =>
-                                    const SingupPage(),
-                              ));
-                        },
-                        onHover: (hover) {},
-                        style: ButtonStyle(
-                          foregroundColor:
-                              MaterialStateProperty.all<Color>(Colors.blue),
-                          backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.white),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                      side: BorderSide.none)),
-                        ),
-                        child: const Text("Vamos lá!",
-                            style: TextStyle(fontSize: 20)),
+          child: Column(children: [
+            const Expanded(
+              flex: 3,
+              child: PaulaTitleComponent(),
+            ),
+            Expanded(
+                flex: 8,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                        'assets/images/Avatar-Maker(2).png',
+                        width: MediaQuery.of(context).size.width - 150,
                       ),
-                    ),
-                  ],
-                ),
-              ))
-        ]),
+                      const DialogTextBox(
+                        TextContent:
+                            "Para começar vamos precisar de algumas informações suas, \ntudo bem?",
+                        audioUrl: 'paula02.ogg',
+                      ),
+                      const SizedBox(height: 30),
+                      SizedBox(
+                        width: 140,
+                        height: 40,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      const SingupPage(),
+                                ));
+                          },
+                          onHover: (hover) {},
+                          style: ButtonStyle(
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.blue),
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            shape:
+                                MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                        side: BorderSide.none)),
+                          ),
+                          child: const Text("Vamos lá!",
+                              style: TextStyle(fontSize: 20)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ))
+          ]),
+        ),
       ),
     );
   }
