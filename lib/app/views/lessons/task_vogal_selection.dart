bd45f348c51a3ac44ddcac262beda8f19f5b4145
PEDRO LUCAS SANTANA <pedrolucassantana@gmail.com>
2022-09-06 17:41:30 -0300

    tela_task_vogal_selection

--- lib/app/views/lessons/task_vogal_selection.dart
+++ lib/app/views/lessons/task_vogal_selection.dart
@@ -8,75 +8,132 @@ class TaskVogalSelection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return 
-        Container(
-                height: 100.0,
-                decoration: const BoxDecoration(
-                    color: Color.fromRGBO(37, 85, 124, 1),
-                    borderRadius: BorderRadius.all(Radius.circular(15))),
-                child: const Center(
-                  child: Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Text(
-                      "Selecione as vogais: ",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w300,
-                      ),  
-                    ),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Container(
+            height: 100.0,
+            decoration: const BoxDecoration(
+                color: Color.fromRGBO(37, 85, 124, 1),
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+            child: const Center(
+              child: Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Text(
+                  "Selecione as vogais: ",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w300,
+                    decoration: TextDecoration.none,
                   ),
                 ),
-              // ignore: dead_code   
-         
-              child: Row(
-                children: const [
-                  SelectLetterButton(
-                    letter: 'E',
-                  ),
-                  SelectLetterButton(
-                    letter: 'S',
-                  ),
-                  SelectLetterButton(
-                    letter: 'C',
-                  ),
-                  SelectLetterButton(
-                    letter: 'A',
-                  ),
-                  SelectLetterButton(
-                    letter: 'D',
-                  ),
-                  SelectLetterButton(
-                    letter: 'A',
-                  ),
-                ],
               ),
-
-              child: Row(
-                children: const[
-                  SelectLetterButton(
-                    letter: 'F',
-                  ),
-                  SelectLetterButton(
-                    letter: 'A',
-                  ),
-                  SelectLetterButton(
-                    letter: 'C',
-                  ),
-                  SelectLetterButton(
-                    letter: 'A',
-                  ),
-                ]
-
-              child: const Center(
-                  child: SizedBox(
-                    width: 180,
-                    height: 50,
-                    child: ButtonNext(pageWidget: TaskPage()),
-                  ),
-              ),
+            ),
           ),
-        );
+        ),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Container(
+            height: 200.0,
+            decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 105, 112, 118),
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 18.0),
+                  child: Container(
+                      height: 87.0, child: Image.asset('images/escada.png')),
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      SelectLetterButton(
+                        letter: 'E',
+                      ),
+                      SelectLetterButton(
+                        letter: 'S',
+                      ),
+                      SelectLetterButton(
+                        letter: 'C',
+                      ),
+                      SelectLetterButton(
+                        letter: 'A',
+                      ),
+                      SelectLetterButton(
+                        letter: 'D',
+                      ),
+                      SelectLetterButton(
+                        letter: 'A',
+                      ),
+                    ])
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Container(
+            height: 200.0,
+            decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 105, 112, 118),
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 18.0),
+                  child: Container(
+                      height: 87.0, child: Image.asset('images/uva.png')),
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      SelectLetterButton(
+                        letter: 'F',
+                      ),
+                      SelectLetterButton(
+                        letter: 'A',
+                      ),
+                      SelectLetterButton(
+                        letter: 'C',
+                      ),
+                      SelectLetterButton(
+                        letter: 'A',
+                      ),
+                    ]),
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Container(
+            height: 50.0,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.blue),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          side: BorderSide.none))),
+              child: const Text('CONTINUAR',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  )),
+              onPressed: () {},
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
 
