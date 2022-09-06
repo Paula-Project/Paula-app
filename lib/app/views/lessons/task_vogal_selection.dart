065a49c2d434fcf46d2c1465a5100f416e7ca94d
PEDRO LUCAS SANTANA <pedrolucassantana@gmail.com>
2022-09-06 14:36:41 -0300

    Task_vogal_selection_pedro

--- lib/app/views/lessons/task_vogal_selection.dart
+++ lib/app/views/lessons/task_vogal_selection.dart
@@ -1,38 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:paula/app/views/lessons/task_page.dart';
+
+import '../components/ButtonNext.dart';
 
 class TaskVogalSelection extends StatelessWidget {
   const TaskVogalSelection({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: const BoxDecoration(color: Colors.green),
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Center(
-          child: Row(
-            children: const [
-              SelectLetterButton(
-                letter: 'E',
+    return 
+        Container(
+                height: 100.0,
+                decoration: const BoxDecoration(
+                    color: Color.fromRGBO(37, 85, 124, 1),
+                    borderRadius: BorderRadius.all(Radius.circular(15))),
+                child: const Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(12.0),
+                    child: Text(
+                      "Selecione as vogais: ",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w300,
+                      ),  
+                    ),
+                  ),
+                ),
+              // ignore: dead_code   
+         
+              child: Row(
+                children: const [
+                  SelectLetterButton(
+                    letter: 'E',
+                  ),
+                  SelectLetterButton(
+                    letter: 'S',
+                  ),
+                  SelectLetterButton(
+                    letter: 'C',
+                  ),
+                  SelectLetterButton(
+                    letter: 'A',
+                  ),
+                  SelectLetterButton(
+                    letter: 'D',
+                  ),
+                  SelectLetterButton(
+                    letter: 'A',
+                  ),
+                ],
               ),
-              SelectLetterButton(
-                letter: 'S',
+
+              child: Row(
+                children: const[
+                  SelectLetterButton(
+                    letter: 'F',
+                  ),
+                  SelectLetterButton(
+                    letter: 'A',
+                  ),
+                  SelectLetterButton(
+                    letter: 'C',
+                  ),
+                  SelectLetterButton(
+                    letter: 'A',
+                  ),
+                ]
+
+              child: const Center(
+                  child: SizedBox(
+                    width: 180,
+                    height: 50,
+                    child: ButtonNext(pageWidget: TaskPage()),
+                  ),
               ),
-              SelectLetterButton(
-                letter: 'C',
-              ),
-              SelectLetterButton(
-                letter: 'A',
-              ),
-              SelectLetterButton(
-                letter: 'D',
-              ),
-              SelectLetterButton(
-                letter: 'A',
-              ),
-            ],
           ),
-        ));
+        );
   }
 }
 
