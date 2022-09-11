e94529b232ab7c42f4b5dda818a2da43a05e0aa1
emersonteles <emersonteles21@gmail.com>
2022-09-11 19:45:25 -0300

    adding new assets

--- lib/app/model/words.dart
+++ lib/app/model/words.dart
@@ -1,5 +1,6 @@
 import 'package:paula/app/model/word.dart';
 
+//set words assets from /assets/words
 class Words {
   List<Word> words = [];
   List<String> wordsList = [
@@ -13,7 +14,21 @@ class Words {
     'Uva',
     'Avião',
     'Abacaxi',
-    'lata'
+    'Lata',
+    'bebida',
+    'boia',
+    'bola',
+    'caneta',
+    'capa',
+    'cavalo',
+    'dados',
+    'envelope',
+    'iguana',
+    'ioio',
+    'meia',
+    'onibus',
+    'ovo',
+    'padaria'
   ];
   List<String> soundsList = [
     'abajur.mp3',
@@ -26,7 +41,21 @@ class Words {
     'uva.mp3',
     'aviao.mp3',
     'abacaxi.mp3',
-    'lata.mp3'
+    'lata.mp3',
+    'bebida.mp3',
+    'boia.mp3',
+    'bola.mp3',
+    'caneta.mp3',
+    'capa.mp3',
+    'cavalo.mp3',
+    'dados.mp3',
+    'envelope.mp3',
+    'iguana.mp3',
+    'ioio.mp3',
+    'meia.mp3',
+    'onibus.mp3',
+    'ovo.mp3',
+    'padaria.mp3'
   ];
   List<String> imagesList = [
     'abajur.png',
@@ -39,7 +68,21 @@ class Words {
     'uva.png',
     'aviao.png',
     'abacaxi.png',
-    'lata.png'
+    'lata.png',
+    'bebida.png',
+    'boia.png',
+    'bola.png',
+    'caneta.png',
+    'capa.png',
+    'cavalo.png',
+    'dados.png',
+    'envelope.png',
+    'iguana.png',
+    'ioio.png',
+    'meia.png',
+    'onibus.png',
+    'ovo.png',
+    'padaria.png'
   ];
   void populate() {
     for (int i = 0; i < wordsList.length; i++) {
@@ -56,9 +99,7 @@ class Words {
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this.words != null) {
-      data['words'] = this.words.map((v) => v.toJson()).toList();
-    }
+    data['words'] = this.words.map((v) => v.toJson()).toList();
     return data;
   }
 }
