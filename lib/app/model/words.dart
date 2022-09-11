0010c2a02a0477a226eb8bd0dae59415a7b01a02
emersonteles <emersonteles21@gmail.com>
2022-09-11 20:01:40 -0300

    adding onibus audio  asset

--- lib/app/model/words.dart
+++ lib/app/model/words.dart
@@ -4,31 +4,31 @@ import 'package:paula/app/model/word.dart';
 class Words {
   List<Word> words = [];
   List<String> wordsList = [
-    'Abajur',
-    'Antena',
-    'Apito',
-    'Arroz',
-    'Árvore',
-    'Escada',
-    'Oculos',
-    'Uva',
-    'Avião',
-    'Abacaxi',
-    'Lata',
-    'bebida',
-    'boia',
-    'bola',
-    'caneta',
-    'capa',
-    'cavalo',
-    'dados',
-    'envelope',
-    'iguana',
-    'ioio',
-    'meia',
-    'onibus',
-    'ovo',
-    'padaria'
+    'Abajur', // 0
+    'Antena', // 1
+    'Apito', // 2
+    'Arroz', // 3
+    'Árvore', // 4
+    'Escada', // 5
+    'Oculos', // 6
+    'Uva', // 7
+    'Avião', // 8
+    'Abacaxi', // 9
+    'Lata', // 10
+    'bebida', // 11
+    'boia', // 12
+    'bola', // 13
+    'caneta', // 14
+    'capa', // 15
+    'cavalo', // 16
+    'dados', // 17
+    'envelope', // 18
+    'iguana', // 19
+    'ioio', // 20
+    'meia', // 21
+    'ovo', // 22
+    'padaria' // 23
+        'onibus', // 24
   ];
   List<String> soundsList = [
     'abajur.mp3',
@@ -53,9 +53,9 @@ class Words {
     'iguana.mp3',
     'ioio.mp3',
     'meia.mp3',
-    'onibus.mp3',
     'ovo.mp3',
     'padaria.mp3'
+        'onibus.mp3',
   ];
   List<String> imagesList = [
     'abajur.png',
@@ -80,9 +80,9 @@ class Words {
     'iguana.png',
     'ioio.png',
     'meia.png',
-    'onibus.png',
     'ovo.png',
     'padaria.png'
+        'onibus.png',
   ];
   void populate() {
     for (int i = 0; i < wordsList.length; i++) {
