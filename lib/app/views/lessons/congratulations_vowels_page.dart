07f9feb0c7d95f3c464fac03aa8dc11ca00ce5a7
Lucas de Pádua <72556348+Padualb@users.noreply.github.com>
2022-09-15 01:22:17 -0300

    Selo vogal page (#43)
    * Criação tela de parabenização módulo vogais
    
    * Corrigindo lesson AEU controller

---
+++ lib/app/views/lessons/congratulations_vowels_page.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../components/ButtonNext.dart';
+import '../home_page.dart';
+
+class CongratulationsVowelsPage extends StatefulWidget {
+  const CongratulationsVowelsPage({Key? key}) : super(key: key);
+
+  @override
+  State<CongratulationsVowelsPage> createState() => _CongratulationsVowelsPageState();
+}
+
+class _CongratulationsVowelsPageState extends State<CongratulationsVowelsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+        ),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30, 100, 30, 10),
+              child: Container(
+                height: (MediaQuery.of(context).size.height) * 0.6,
+                width: (MediaQuery.of(context).size.width) * 0.8,
+                decoration: const BoxDecoration(
+                  color: Color.fromRGBO(37, 85, 124, 1),
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 10.0,
+                      spreadRadius: 2.0,
+                    )
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(3.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Image.asset('assets/images/fireworks.png',
+                              scale: 6.0),
+                          Image.asset('assets/images/fireworks.png',
+                              scale: 6.0),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(8, 40, 8, 0),
+                      child: Column(
+                        children: const [
+                          Text(
+                            "PARABÉNS!!",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              color: Colors.white,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "VOCÊ CONCLUIU O MÓDULO DE VOGAIS",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              color: Colors.white70,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30),
+                      child: Image.asset('images/selos/Selo_vogais.png',
+                          scale: 3.5),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 70.0),
+              child: SizedBox(
+                  width: 350,
+                  height: 50,
+                  child: ButtonNext(
+                    pageWidget: HomePage(),
+                    allowedReturn: false,
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
