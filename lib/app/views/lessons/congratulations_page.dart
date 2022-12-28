6d923b545a386d9fb36c7204238bbe138034f25c
emersonteles <emersonteles21@gmail.com>
2022-12-28 17:26:03 -0300

    feat congrats page

--- lib/app/views/lessons/congratulations_page.dart
+++ lib/app/views/lessons/congratulations_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:paula/app/controllers/module_vowels_controller.dart';
 import 'package:paula/app/views/lessons/lessons_vogais.dart';
 import 'package:paula/app/views//components/ButtonNext.dart';
+import 'package:lottie/lottie.dart';
 
 class CongratulationsPage extends StatefulWidget {
   final ModuleVowelsController moduleVowelsController;
@@ -24,7 +25,14 @@ class _CongratulationsPageState extends State<CongratulationsPage> {
       child: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
-            color: Colors.white,
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color.fromARGB(200, 100, 171, 226),
+                Color.fromARGB(255, 41, 171, 226),
+              ],
+            ),
           ),
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
@@ -32,38 +40,13 @@ class _CongratulationsPageState extends State<CongratulationsPage> {
             children: [
               Padding(
                 padding: const EdgeInsets.fromLTRB(30, 100, 30, 10),
-                child: Container(
-                  height: (MediaQuery.of(context).size.height) * 0.6,
-                  width: (MediaQuery.of(context).size.width) * 0.8,
-                  decoration: const BoxDecoration(
-                    color: Color.fromRGBO(37, 85, 124, 1),
-                    borderRadius: BorderRadius.all(Radius.circular(15)),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey,
-                        blurRadius: 10.0,
-                        spreadRadius: 2.0,
-                      )
-                    ],
-                  ),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(3.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Image.asset('assets/images/fireworks.png',
-                                scale: 6.0),
-                            Image.asset('assets/images/fireworks.png',
-                                scale: 6.0),
-                          ],
-                        ),
-                      ),
+                child: Column(
+                  children: [
+                    Stack(children: [
                       const Padding(
-                        padding: EdgeInsets.fromLTRB(8, 40, 8, 0),
+                        padding: EdgeInsets.fromLTRB(8, 00, 8, 0),
                         child: Text(
-                          "VOCÊ ESTÁ \n INDO MUITO \n BEM!",
+                          "VOCÊ É INCRÍVEL!",
                           textAlign: TextAlign.center,
                           style: TextStyle(
                             fontFamily: 'Poppins',
@@ -74,26 +57,69 @@ class _CongratulationsPageState extends State<CongratulationsPage> {
                         ),
                       ),
                       Padding(
-                        padding: const EdgeInsets.only(top: 30),
-                        child: Image.asset('assets/images/fireworks.png',
-                            scale: 6.0),
+                        padding: const EdgeInsets.all(3.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                                height: 200,
+                                child: Lottie.asset(
+                                    'assets/images/fireworks.json')),
+                          ],
+                        ),
                       ),
-                    ],
-                  ),
+                    ]),
+                    Center(
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          SizedBox(
+                              height: 200,
+                              child:
+                                  Lottie.asset('assets/images/fireworks.json')),
+                          SizedBox(
+                              height: 350,
+                              child: Image.asset(
+                                "assets/images/paula/paula04.png",
+                                alignment: Alignment.center,
+                              )),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
               Container(
                 margin: const EdgeInsets.only(top: 70.0),
                 child: SizedBox(
-                    width: 350,
+                    width: 200,
                     height: 50,
-                    child: ButtonNext(
-                      onPressed: () {},
-                      pageWidget: LessonsVogais(
-                        moduleVowelsController: widget.moduleVowelsController,
-                      ),
-                      allowedReturn: false,
-                    )),
+                    child: TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(
+                              builder: (BuildContext context) => LessonsVogais(
+                                  moduleVowelsController:
+                                      widget.moduleVowelsController),
+                            ),
+                            (route) => false,
+                          );
+                        },
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.blue),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                  side: BorderSide.none),
+                            )),
+                        child: const Text(
+                          "AVANÇAR",
+                          style: TextStyle(fontSize: 20),
+                        ))),
               ),
             ],
           ),
