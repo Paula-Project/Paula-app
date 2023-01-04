22bc16dd9c3a060012cc0a50a741ac168a398d30
Th4lisc <thaliscezar@gmail.com>
2023-01-04 17:22:24 -0300

    adding images

--- lib/app/controllers/task_paranoa_tour_controller.dart
+++ lib/app/controllers/task_paranoa_tour_controller.dart
@@ -14,27 +14,27 @@ class TaskParanoaTourController extends TaskController {
     taskFood = TaskParanoaTourModel(
       title: "Quando estou com fome, penso logo em ir à:",
       audioPath: "paula_paranoaFome.mp3",
-      words: [words.words[40], words.words[41]],
+      words: [words.words[41], words.words[44]],
     );
     taskSunday = TaskParanoaTourModel(
       title: "Todo domingo gosto de ir à:",
       audioPath: "paula_paranoaDomingo.mp3",
-      words: [words.words[40], words.words[41]],
+      words: [words.words[48], words.words[51]],
     );
     taskColors = TaskParanoaTourModel(
       title: "O paranoá é um lugar muito colorido, pois aqui temos:",
       audioPath: "paula_paranoaColorido.mp3",
-      words: [words.words[40], words.words[42]],
+      words: [words.words[42], words.words[50]],
     );
     taskDurst = TaskParanoaTourModel(
       title: "Sempre que estou com sede, tenho muitas distribuidoras para ir:",
       audioPath: "paula_paranoaDistribuidoras.mp3",
-      words: [words.words[39], words.words[41]],
+      words: [words.words[45], words.words[49]],
     );
     taskLake = TaskParanoaTourModel(
       title: "Quando estou com calor, vou sempre no lago paranoá:",
       audioPath: "paula_paranoaLago.mp3",
-      words: [words.words[40], words.words[43]],
+      words: [words.words[46], words.words[47]],
     );
   }
   TaskParanoaTourModel getTaskFood() {
