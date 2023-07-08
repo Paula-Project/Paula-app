e6b854e7f7bd9267a252ff09028ee386f62326a2
Lucas Rodrigues <lucasrodrigues@MacBook-Air-de-Lucas.local>
2023-07-08 19:57:02 -0300

    feat: adding TaskLayout to taskDiphtong

--- lib/app/views/tasks/task_diphthong.dart
+++ lib/app/views/tasks/task_diphthong.dart
@@ -1,7 +1,9 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:paula/app/controllers/lessons/lesson_controller_interface.dart';
 import 'package:paula/app/views/components/audioManager.dart';
+import 'package:paula/app/views/layout/task_layout.dart';
 
 class TaskDiphthong extends StatefulWidget {
   final LessonControllerInterface lessonController;
@@ -43,134 +45,130 @@ class _TaskDiphthongState extends State<TaskDiphthong>
 
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
-    return WillPopScope(
-      onWillPop: () => _onwillpop(),
-      child: Material(
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 50),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                MaterialButton(
-                  onPressed: () {
-                    audioManager.runAudio("audios/paula/paula_ditongos.mp3");
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        "assets/images/paula/paula05.png",
-                        height: height * 0.3,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 10),
-                            child: ClipPath(
-                              clipper: CustomTriangleClipper(),
-                              child: Container(
-                                width: 20,
-                                height: 25,
-                                decoration: const BoxDecoration(
-                                    color: Color.fromARGB(199, 37, 85, 124)),
-                              ),
-                            ),
-                          ),
-                          Stack(children: [
-                            Container(
-                              width: (width - 45) / 2,
-                              height: height * 0.3,
-                              padding: const EdgeInsets.all(15),
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                  color: const Color.fromARGB(199, 37, 85, 124),
-                                  borderRadius: BorderRadius.circular(10)),
-                              child: const Text(
-                                  "Você sabia que com apenas 2 vogais, você já tem uma palavra?",
-                                  style: TextStyle(
-                                      fontSize: 20, color: Colors.white)),
-                            ),
-                            const Positioned(
-                              right: 10,
-                              bottom: 10,
-                              child: Icon(
-                                Icons.spatial_audio_off_sharp,
-                                color: Colors.white,
-                              ),
-                            )
-                          ]),
-                        ],
-                      )
-                    ],
+    return TaskLayout(
+      shouldPop: true,
+      taskProgress: Container(),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            MaterialButton(
+              onPressed: () {
+                audioManager.runAudio("audios/paula/paula_ditongos.mp3");
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/images/paula/paula05.png",
+                    height: height * 0.3,
                   ),
-                ),
-                DiphthongWidget(
-                  letters: const ['o', 'i'],
-                  audioManager: audioManager,
-                  audioPath: 'audios/paula/paula_oi.mp3',
-                ),
-                DiphthongWidget(
-                  letters: const ['a', 'i'],
-                  audioManager: audioManager,
-                  audioPath: 'audios/paula/paula_ai.mp3',
-                ),
-                DiphthongWidget(
-                  letters: const ['e', 'i'],
-                  audioManager: audioManager,
-                  audioPath: 'audios/paula/paula_ei.mp3',
-                ),
-                DiphthongWidget(
-                  letters: const ['u', 'i'],
-                  audioManager: audioManager,
-                  audioPath: 'audios/paula/paula_ui.mp3',
-                ),
-                DiphthongWidget(
-                  letters: const ['o', 'u'],
-                  audioManager: audioManager,
-                  audioPath: 'audios/paula/paula_ou.mp3',
-                ),
-                SizedBox(
-                    height: 40.0,
-                    width: 150.0,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).pushAndRemoveUntil(
-                            PageTransition(
-                                type: PageTransitionType.rightToLeft,
-                                child: widget.lessonController.nextTask()),
-                            (route) => false);
-                      },
-                      style: ButtonStyle(
-                          foregroundColor:
-                              MaterialStateProperty.all<Color>(Colors.white),
-                          backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.blue),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                      side: BorderSide.none))),
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: const [
-                            Text("AVANÇAR",
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w600,
-                                )),
-                            Icon(
-                              Icons.arrow_forward,
-                              size: 26,
-                              color: Colors.white,
-                            )
-                          ]),
-                    ))
-              ]),
-        ),
-      ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: ClipPath(
+                          clipper: CustomTriangleClipper(),
+                          child: Container(
+                            width: 20,
+                            height: 25,
+                            decoration: const BoxDecoration(
+                                color: Color.fromARGB(199, 37, 85, 124)),
+                          ),
+                        ),
+                      ),
+                      Stack(children: [
+                        Container(
+                          width: (width - 45) / 2,
+                          height: height * 0.3,
+                          padding: const EdgeInsets.all(15),
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(199, 37, 85, 124),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: const AutoSizeText(
+                              "Você sabia que com apenas 2 vogais, você já tem uma palavra?",
+                              textScaleFactor: 1,
+                              style: TextStyle(
+                                  fontSize: 20, color: Colors.white)),
+                        ),
+                        const Positioned(
+                          right: 10,
+                          bottom: 10,
+                          child: Icon(
+                            Icons.spatial_audio_off_sharp,
+                            color: Colors.white,
+                          ),
+                        )
+                      ]),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            DiphthongWidget(
+              letters: const ['o', 'i'],
+              audioManager: audioManager,
+              audioPath: 'audios/paula/paula_oi.mp3',
+            ),
+            DiphthongWidget(
+              letters: const ['a', 'i'],
+              audioManager: audioManager,
+              audioPath: 'audios/paula/paula_ai.mp3',
+            ),
+            DiphthongWidget(
+              letters: const ['e', 'i'],
+              audioManager: audioManager,
+              audioPath: 'audios/paula/paula_ei.mp3',
+            ),
+            DiphthongWidget(
+              letters: const ['u', 'i'],
+              audioManager: audioManager,
+              audioPath: 'audios/paula/paula_ui.mp3',
+            ),
+            DiphthongWidget(
+              letters: const ['o', 'u'],
+              audioManager: audioManager,
+              audioPath: 'audios/paula/paula_ou.mp3',
+            ),
+            SizedBox(
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushAndRemoveUntil(
+                        PageTransition(
+                            type: PageTransitionType.rightToLeft,
+                            child: widget.lessonController.nextTask()),
+                        (route) => false);
+                  },
+                  style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.blue),
+                      shape:
+                          MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  side: BorderSide.none))),
+                  child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text("AVANÇAR",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            )),
+                        Icon(
+                          Icons.arrow_forward,
+                          size: 26,
+                          color: Colors.white,
+                        )
+                      ]),
+                ))
+          ]),
     );
   }
 }
@@ -196,11 +194,19 @@ class DiphthongWidget extends StatelessWidget {
         children: [
           SizedBox(
             width: 90,
-            child: Text("${letters[0]} + ${letters[1]}",
-                style: const TextStyle(fontSize: 35, color: Colors.black)),
+            child: AutoSizeText(
+              "${letters[0]} + ${letters[1]}",
+              textScaleFactor: 1,
+              style: const TextStyle(
+                fontSize: 35, 
+                color: Colors.black
+              )
+            ),
           ),
-          const Text(" = ",
-              style: TextStyle(fontSize: 35, color: Colors.black)),
+          const AutoSizeText(
+            " = ",
+            textScaleFactor: 1,
+            style: TextStyle(fontSize: 35, color: Colors.black)),
           MaterialButton(
             onPressed: () {
               audioManager.runAudio(audioPath);
@@ -216,8 +222,10 @@ class DiphthongWidget extends StatelessWidget {
                 mainAxisSize: MainAxisSize.max,
                 children: [
                   Center(
-                    child: Text("${letters[0]}${letters[1]}",
-                        style:
+                    child: AutoSizeText(
+                      "${letters[0]}${letters[1]}",
+                      textScaleFactor: 1,
+                      style:
                             const TextStyle(fontSize: 35, color: Colors.white)),
                   ),
                   const Icon(
