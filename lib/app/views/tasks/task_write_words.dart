413b0fc8b1203a5e2c3f6e9cb2cddcfe97ae5954
Lucas Rodrigues <lucasrodrigues@MacBook-Air-de-Lucas.local>
2023-07-08 19:49:30 -0300

    style: Fix complete words tasks responsiveness

--- lib/app/views/tasks/task_write_words.dart
+++ lib/app/views/tasks/task_write_words.dart
@@ -4,9 +4,11 @@ import 'package:paula/app/controllers/lessons/lesson_controller_interface.dart';
 import 'package:paula/app/views/components/BoxDialog.dart';
 import 'package:paula/app/views/components/audioManager.dart';
 import 'package:paula/app/views/components/exitDialog.dart';
+import 'package:paula/app/views/components/taskTitle.dart';
 import 'package:paula/app/views/components/task_progress.dart';
 import 'package:paula/app/controllers/tasks/task_complete_word_controller.dart';
 import 'package:paula/app/model/task_complete_word_model.dart';
+import 'package:paula/app/views/layout/task_layout.dart';
 
 class TaskWriteWords extends StatefulWidget {
   final LessonControllerInterface lessonController;
@@ -34,7 +36,6 @@ class _TaskWriteWordsState extends State<TaskWriteWords>
   @override
   void initState() {
     WidgetsBinding.instance.addObserver(this);
-    audioManager.runAudio("audios/paula/${widget.task.audio}");
     super.initState();
   }
 
@@ -59,12 +60,13 @@ class _TaskWriteWordsState extends State<TaskWriteWords>
           color: Color.fromRGBO(209, 220, 221, 1),
         ),
         alignment: Alignment.center,
-        child: Text(
+        child: AutoSizeText(
           letter,
           textAlign: TextAlign.center,
+          textScaleFactor: 1,
           style: const TextStyle(
             color: Colors.black,
-            fontSize: 30,
+            fontSize: 28,
             fontWeight: FontWeight.bold,
           ),
         ),
@@ -118,213 +120,156 @@ class _TaskWriteWordsState extends State<TaskWriteWords>
   Widget build(BuildContext context) {
     var LetterList = widget.task.lessonVowels;
     LetterList.sort();
-    return WillPopScope(
-      onWillPop: (() => exitDialog(context)),
-      child: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            color: Colors.white,
-          ),
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 40.0, horizontal: 15.0),
+    return TaskLayout(
+      shouldPop: true,
+      taskProgress: TaskProgress(
+        tasksNumber: widget.lessonController.getTaskQuantity(),
+        correctAnswer:
+            widget.lessonController.getTaskCorrectAnswers(),
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          TaskTitle(title: widget.task.title, audio: widget.task.audio),
+          Expanded(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               mainAxisSize: MainAxisSize.max,
               children: [
-                TaskProgress(
-                  tasksNumber: widget.lessonController.getTaskQuantity(),
-                  correctAnswer:
-                      widget.lessonController.getTaskCorrectAnswers(),
-                ),
-                const SizedBox(height: 10.0),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: MaterialButton(
-                    onPressed: () {
-                      audioManager
-                          .runAudio("audios/paula/${widget.task.audio}");
-                    },
-                    child: Stack(children: [
-                      Container(
-                        height: 100.0,
-                        decoration: const BoxDecoration(
-                            color: Color.fromRGBO(37, 85, 124, 1),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(15))),
-                        child: Center(
-                          child: Padding(
-                            padding: const EdgeInsets.all(12.0),
-                            child: AutoSizeText(
-                              widget.task.title,
-                              textAlign: TextAlign.center,
-                              style: const TextStyle(
-                                fontSize: 25,
-                                fontWeight: FontWeight.w300,
-                              ),
-                              minFontSize: 15,
-                            ),
-                          ),
-                        ),
-                      ),
-                      const Positioned(
-                        right: 10,
-                        bottom: 10,
-                        child: Icon(
-                          Icons.spatial_audio_off_sharp,
-                          color: Colors.white,
-                        ),
-                      )
-                    ]),
-                  ),
-                ),
-                Expanded(
+                Container(
+                  decoration: const BoxDecoration(
+                      color: Color.fromRGBO(209, 220, 221, 1),
+                      borderRadius:
+                          BorderRadius.all(Radius.circular(15))),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Container(
-                          decoration: const BoxDecoration(
-                              color: Color.fromRGBO(209, 220, 221, 1),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15))),
-                          child: Column(
-                              children: widget.task.words
-                                  .map(
-                                    (word) => MaterialButton(
-                                      onPressed: (() {
-                                        audioManager.runAudio(
-                                            "audios/words/${word.soundPath}");
-                                      }),
+                      children: widget.task.words
+                          .map(
+                            (word) => MaterialButton(
+                              onPressed: (() {
+                                audioManager.runAudio(
+                                    "audios/words/${word.soundPath}");
+                              }),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Column(
+                                  children: [
+                                    SizedBox(
+                                      height: 90,
                                       child: Padding(
-                                        padding: const EdgeInsets.all(10.0),
-                                        child: Column(
-                                          children: [
-                                            SizedBox(
-                                              height: 120,
-                                              child: Padding(
-                                                padding: const EdgeInsets.only(
-                                                    bottom: 8.0),
-                                                child: Image.asset(
-                                                    'assets/images/words/${word.imagePath}'),
-                                              ),
-                                            ),
-                                            SizedBox(
-                                              width: double.infinity,
-                                              child: Row(
-                                                mainAxisAlignment:
-                                                    MainAxisAlignment
-                                                        .spaceEvenly,
-                                                children: makeWord(word.text),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
+                                        padding: const EdgeInsets.only(
+                                            bottom: 8.0),
+                                        child: Image.asset(
+                                            'assets/images/words/${word.imagePath}'),
                                       ),
                                     ),
-                                  )
-                                  .toList()),
-                        ),
-                      ),
-                      FittedBox(
-                        fit: BoxFit.fitWidth,
-                        child: Padding(
-                          padding:
-                              const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children:
-                                  LetterList.map((vowel) => LetterBox(vowel))
-                                      .toList()),
-                        ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(top: 15.0),
-                        child: SizedBox(
-                            width: 200,
-                            height: 40,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SizedBox(
-                                  height: 50.0,
-                                  width: 150.0,
-                                  child: ElevatedButton(
-                                    style: ButtonStyle(
-                                        foregroundColor:
-                                            MaterialStateProperty.all<Color>(
-                                                Colors.white),
-                                        shape: MaterialStateProperty.all<
-                                                RoundedRectangleBorder>(
-                                            RoundedRectangleBorder(
-                                                borderRadius:
-                                                    BorderRadius.circular(10),
-                                                side: BorderSide.none))),
-                                    child: const Text('VERIFICAR',
-                                        style: TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.w600,
-                                        )),
-                                    onPressed: () {
-                                      widget.taskController
-                                          .makeAnswers(widget.task);
-
-                                      isCorrect =
-                                          widget.taskController.verifyAnswer();
-                                      widget.lessonController
-                                          .verifyAnswerNonControlled(isCorrect);
-                                      widget.taskController.reset();
-                                      showGeneralDialog(
-                                        barrierColor:
-                                            Colors.black.withOpacity(0.5),
-                                        transitionDuration:
-                                            const Duration(milliseconds: 300),
-                                        barrierDismissible: false,
-                                        barrierLabel: '',
-                                        context: context,
-                                        pageBuilder:
-                                            (context, animation1, animation2) {
-                                          return widget;
-                                        },
-                                        transitionBuilder:
-                                            (context, a1, a2, widget) {
-                                          final curvedValue = Curves.easeInOut
-                                                  .transform(a1.value) -
-                                              1;
-
-                                          return Transform(
-                                            transform:
-                                                Matrix4.translationValues(0.0,
-                                                    (curvedValue * -300), 0.0),
-                                            child: Opacity(
-                                              opacity: a1.value,
-                                              child: BoxDialog(
-                                                  controller: this
-                                                      .widget
-                                                      .lessonController,
-                                                  feedback: isCorrect,
-                                                  resposta:
-                                                      '${this.widget.task.words[0].text.toUpperCase()} '),
-                                            ),
-                                          );
-                                        },
-                                      );
-                                    },
-                                  ),
+                                    SizedBox(
+                                      width: double.infinity,
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment
+                                                .spaceEvenly,
+                                        children: makeWord(word.text),
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ],
-                            )),
-                      ),
-                    ],
+                              ),
+                            ),
+                          )
+                          .toList()),
+                ),
+                FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children:
+                            LetterList.map((vowel) => LetterBox(vowel))
+                                .toList()),
                   ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 15.0),
+                  child: SizedBox(
+                      width: 200,
+                      height: 40,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 50.0,
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                  foregroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.white),
+                                  shape: MaterialStateProperty.all<
+                                          RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          side: BorderSide.none))),
+                              child: const Text('VERIFICAR',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600,
+                                  )),
+                              onPressed: () {
+                                widget.taskController
+                                    .makeAnswers(widget.task);
+
+                                isCorrect =
+                                    widget.taskController.verifyAnswer();
+                                widget.lessonController
+                                    .verifyAnswerNonControlled(isCorrect);
+                                widget.taskController.reset();
+                                showGeneralDialog(
+                                  barrierColor:
+                                      Colors.black.withOpacity(0.5),
+                                  transitionDuration:
+                                      const Duration(milliseconds: 300),
+                                  barrierDismissible: false,
+                                  barrierLabel: '',
+                                  context: context,
+                                  pageBuilder:
+                                      (context, animation1, animation2) {
+                                    return widget;
+                                  },
+                                  transitionBuilder:
+                                      (context, a1, a2, widget) {
+                                    final curvedValue = Curves.easeInOut
+                                            .transform(a1.value) -
+                                        1;
+
+                                    return Transform(
+                                      transform:
+                                          Matrix4.translationValues(0.0,
+                                              (curvedValue * -300), 0.0),
+                                      child: Opacity(
+                                        opacity: a1.value,
+                                        child: BoxDialog(
+                                            controller: this
+                                                .widget
+                                                .lessonController,
+                                            feedback: isCorrect,
+                                            resposta:
+                                                '${this.widget.task.words[0].text.toUpperCase()} '),
+                                      ),
+                                    );
+                                  },
+                                );
+                              },
+                            ),
+                          ),
+                        ],
+                      )),
                 ),
               ],
             ),
           ),
-        ),
+        ],
       ),
     );
   }
@@ -353,13 +298,14 @@ class LetterBox extends StatelessWidget {
         ),
         alignment: Alignment.center,
         margin: const EdgeInsets.all(5),
-        child: Text(
+        child: AutoSizeText(
           letter,
           textAlign: TextAlign.center,
+          textScaleFactor: 1,
           style: const TextStyle(
             color: Colors.white,
             decoration: TextDecoration.none,
-            fontSize: 40,
+            fontSize: 38,
             fontWeight: FontWeight.bold,
           ),
         ),
