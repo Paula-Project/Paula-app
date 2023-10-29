6cdd723793cb42ee1f2853f4daf6721b4597a84c
EmersonTeles <emersonteles21@gmail.com>
2023-10-29 20:59:18 -0300

    feat: Connect ia Api

--- lib/app/views/tasks/task_draw_letter.dart
+++ lib/app/views/tasks/task_draw_letter.dart
@@ -1,22 +1,39 @@
-import 'dart:ui';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:paula/app/controllers/lessons/lesson_controller_interface.dart';
+import 'package:paula/app/controllers/web/ia_controller.dart';
+import 'package:paula/app/http/ia_client.dart';
+import 'package:paula/app/views/components/BoxDialog.dart';
 import 'package:paula/app/views/components/audioManager.dart';
 import 'package:paula/app/views/components/drawing/drawing_point.dart';
 import 'package:paula/app/views/components/taskTitle.dart';
 import 'package:paula/app/views/components/task_progress.dart';
 import 'package:paula/app/views/layout/task_layout.dart';
 
-class DrawingBoard extends StatefulWidget {
+class TaskDrawLetter extends StatefulWidget {
+  final LessonControllerInterface lessonController;
+  final String letter;
+  const TaskDrawLetter(
+      {super.key, required this.lessonController, required this.letter});
+
   @override
-  _DrawingBoardState createState() => _DrawingBoardState();
+  _TaskDrawLetterState createState() => _TaskDrawLetterState();
 }
 
-class _DrawingBoardState extends State<DrawingBoard>
+class _TaskDrawLetterState extends State<TaskDrawLetter>
     with WidgetsBindingObserver {
-  Color selectedColor = Colors.red;
-  double strokeWidth = 5;
+  Color selectedColor = Colors.black;
+  double strokeWidth = 10;
   List<dynamic> drawingPoints = [];
   AudioManager audioManager = AudioManager();
+  final loading = ValueNotifier<bool>(false);
+  bool hasTouched = false;
+
   @override
   void initState() {
     WidgetsBinding.instance.addObserver(this);
@@ -34,11 +51,14 @@ class _DrawingBoardState extends State<DrawingBoard>
   Widget build(BuildContext context) {
     return TaskLayout(
       shouldPop: false,
-      taskProgress: const TaskProgress(tasksNumber: 5, correctAnswer: 4),
+      taskProgress: TaskProgress(
+        tasksNumber: widget.lessonController.getTaskQuantity(),
+        correctAnswer: widget.lessonController.getTaskCorrectAnswers(),
+      ),
       body: Column(
         children: [
           TaskTitle(
-              title: "Escreva a Letra A no espaço abaixo:",
+              title: "Escreva a Letra ${widget.letter} no espaço abaixo:",
               audio: "assets/audios/vogal_selection.mp3",
               audioManager: audioManager),
           Expanded(
@@ -59,6 +79,7 @@ class _DrawingBoardState extends State<DrawingBoard>
                         ),
                       );
                     });
+                    hasTouched = true;
                   },
                   onPanUpdate: (details) {
                     setState(() {
@@ -103,7 +124,10 @@ class _DrawingBoardState extends State<DrawingBoard>
                         backgroundColor: MaterialStateProperty.all<Color>(
                             const Color.fromARGB(255, 244, 67, 54)),
                       ),
-                      onPressed: () => setState(() => drawingPoints = []),
+                      onPressed: () {
+                        setState(() => drawingPoints = []);
+                        hasTouched = false;
+                      },
                       icon: const Icon(Icons.clear),
                       label: const Text("Limpar"),
                     ),
@@ -115,27 +139,42 @@ class _DrawingBoardState extends State<DrawingBoard>
                     style: ButtonStyle(
                         foregroundColor:
                             MaterialStateProperty.all<Color>(Colors.white),
-                        backgroundColor: /*widget.task.cardSelected.isNotEmpty
-                            ? */
-                            MaterialStateProperty.all<Color>(Colors.blue)
-                        /*: MaterialStateProperty.all<Color>(Colors.grey),
-                        */
-                        ,
+                        backgroundColor: hasTouched
+                            ? MaterialStateProperty.all<Color>(Colors.blue)
+                            : MaterialStateProperty.all<Color>(Colors.grey),
                         shape:
                             MaterialStateProperty.all<RoundedRectangleBorder>(
                                 RoundedRectangleBorder(
                                     borderRadius: BorderRadius.circular(10),
                                     side: BorderSide.none))),
-                    child: const Text('VERIFICAR',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600,
-                        )),
-                    onPressed: () {
-                      /*
-                      if (widget.task.cardSelected.isNotEmpty) {
-                        widget.lessonController
-                            .verifyAnswer(widget.task, widget.taskController);
+                    child: AnimatedBuilder(
+                        animation: loading,
+                        builder: (context, snapshot) {
+                          return loading.value
+                              ? const SizedBox(
+                                  height: 20,
+                                  width: 20,
+                                  child: CircularProgressIndicator(
+                                    color: Colors.white,
+                                  ))
+                              : const Text('VERIFICAR',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600,
+                                  ));
+                        }),
+                    onPressed: () async {
+                      if (hasTouched) {
+                        Uint8List? imageBytes =
+                            await DrawingToImage.convertToImage(
+                                drawingPoints,
+                                MediaQuery.of(context).size.width,
+                                MediaQuery.of(context).size.height / 2);
+                        loading.value = !loading.value;
+                        var response =
+                            await IAController().verifyAnswer(imageBytes);
+                        loading.value = !loading.value;
+                        // ignore: use_build_context_synchronously
                         showGeneralDialog(
                           barrierColor: Colors.black.withOpacity(0.5),
                           transitionDuration: const Duration(milliseconds: 300),
@@ -148,17 +187,22 @@ class _DrawingBoardState extends State<DrawingBoard>
                           transitionBuilder: (context, a1, a2, widget) {
                             final curvedValue =
                                 Curves.easeInOut.transform(a1.value) - 1;
-          
+
                             return Transform(
                               transform: Matrix4.translationValues(
                                   0.0, (curvedValue * -300), 0.0),
-                              child:
-                                  Opacity(opacity: a1.value, child: Container()),
+                              child: Opacity(
+                                opacity: a1.value,
+                                child: BoxDialog(
+                                    controller: this.widget.lessonController,
+                                    feedback: response == this.widget.letter,
+                                    resposta:
+                                        "Letra ${this.widget.letter}\n e você escreveu a Letra ${response}"),
+                              ),
                             );
                           },
                         );
                       }
-                    */
                     },
                   ),
                 ),
@@ -182,6 +226,10 @@ class _DrawingPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    canvas.drawRect(
+      Rect.fromPoints(Offset(0, 0), Offset(size.width, size.height)),
+      Paint()..color = Colors.white,
+    );
     for (int i = 0; i < drawingPoints.length - 1; i++) {
       if (drawingPoints[i] != null && drawingPoints[i + 1] != null) {
         // Verifique se os pontos de desenho estão dentro das dimensões limitadas
@@ -208,15 +256,68 @@ class _DrawingPainter extends CustomPainter {
           offsetsList.add(drawingPoints[i].offset);
 
           canvas.drawPoints(
-            PointMode.points,
+            ui.PointMode.points,
             offsetsList,
             drawingPoints[i].paint,
           );
         }
       }
     }
+    _DrawingPainter(this.drawingPoints, this.limitedWidth, this.limitedHeight);
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+}
+
+class DrawingToImage {
+  static Future<Uint8List?> convertToImage(List<dynamic> drawingPoints,
+      double limitedWidth, double limitedHeight) async {
+    final recorder = ui.PictureRecorder();
+    final canvas = Canvas(recorder,
+        Rect.fromPoints(Offset(0, 0), Offset(limitedWidth, limitedHeight)));
+
+    final painter = _DrawingPainter(drawingPoints, limitedWidth, limitedHeight);
+    painter.paint(canvas, Size(limitedWidth, limitedHeight));
+
+    final picture = recorder.endRecording();
+    final img = await picture.toImage(
+      limitedWidth.toInt(),
+      limitedHeight.toInt(),
+    );
+    final byteData = await img.toByteData(format: ui.ImageByteFormat.png);
+
+    if (byteData != null) {
+      return byteData.buffer.asUint8List();
+    } else {
+      return null;
+    }
+  }
+}
+
+class ImageSaver {
+  static Future<String> saveImage(Uint8List imageBytes) async {
+    final directory =
+        await getExternalStorageDirectory(); // Ou use getApplicationDocumentsDirectory() para um diretório interno ao aplicativo.
+    final imagePath =
+        '${directory!.path}/Letras/${DateTime.now().hashCode}.png';
+
+    final newDirectory = Directory('${directory.path}/Letras');
+    if (!(await newDirectory.exists())) {
+      // Verifica se a pasta não existe e a cria se necessário
+      newDirectory.create(recursive: true).then((Directory newDirectory) {
+        print('Pasta criada: ${newDirectory.path}');
+        final imageFile = File(imagePath);
+        imageFile.writeAsBytesSync(
+            imageBytes); // Salva o arquivo na pasta recém-criada
+        print('Arquivo salvo em: $imagePath');
+      });
+    } else {
+      final imageFile = File(imagePath);
+      imageFile
+          .writeAsBytesSync(imageBytes); // Salva o arquivo na pasta existente
+      print('Arquivo salvo em: $imagePath');
+    }
+    return imagePath;
+  }
 }
